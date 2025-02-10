fd7e5d21a165246e253b7362f98121b3c91a1b4e
Ariiiiiii <133253988+AriGL05@users.noreply.github.com>
2025-02-09 18:51:47 -0600

    Removed unnecessary id and class in index

--- js/index.js
+++ js/index.js
@@ -5,7 +5,6 @@ let time = 300;
 
 const animation_1 = () => {
    document.querySelector("#intro_logo").classList.add("d-none");
-   // document.querySelector("#hero").classList.remove("d-none");
 }
 const animation_2 = () => {
    document.querySelector("#hero").classList.add("fade_in");
@@ -25,8 +24,6 @@ window.onscroll = function () {
    let position = window.pageYOffset || document.documentElement.scrollTop;
    let img_hamburger_1 = document.getElementById("img_hamburger_1");
    let section_hamburgers = document.getElementById("section_hamburgers");
-   // console.log(section_hamburgers.style);
-   // img_hamburger_1.style.bottom = `${position * 0.01}px` ;
 }
 //#endregion EFECTO PARALLAX
 
